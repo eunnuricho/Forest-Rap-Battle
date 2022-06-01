cbab4c91d6c69c2131cc9c8f799c69c494551bd1
조은누리 <jounuli@naver.com>
2022-06-01 14:40:34 +0900

    Django Secret Key 삭제 & 유니티 빌드 업로드

--- server/forestrapbattle/my_settings.py
+++ server/forestrapbattle/my_settings.py
@@ -3,15 +3,15 @@ DATABASES = {
                 'ENGINE': 'django.db.backends.mysql',
                 'NAME': 'forest_1.0',
                 # 로컬용
-                # 'USER' : 'root',
-                # 'PASSWORD' : 'donuts',
+                # 'USER' : '',
+                # 'PASSWORD' : '',
                 # 'HOST' : '127.0.0.1',
                 # 'PORT' : '3306',
                 # 서버용
-                'USER': 'root',
-                'PASSWORD': 'forest',
+                'USER': '',
+                'PASSWORD': '',
                 'HOST' : 'k6e204.p.ssafy.io',
                 'PORT' : '3306',
         }
 }
-SECRET_KEY = 'django-insecure-6o1lkec6!%)=lx1+3h_9(k+o#u&^1-l@obs4^76p%j8!m+w)ph'
+SECRET_KEY = ''
